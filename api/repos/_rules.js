dda2ab9674cd9287bfcdce4bddfd33ac96c01b78
Moritz Halke <moritz.orth@bold-ventures.de>
2022-06-08 00:59:38 +0200

    lint

--- api/repos/_rules.js
+++ api/repos/_rules.js
@@ -1,9 +1,9 @@
-import {stringToKebab} from "@/api/repos/_utility";
+import { stringToKebab } from '@/api/repos/_utility';
 
 const sections = (sections) => {
   return {
     sections,
-  }
+  };
 };
 
 const section = (headline, elements) => {
@@ -11,25 +11,24 @@ const section = (headline, elements) => {
     key: `section-${stringToKebab(headline)}`,
     headline,
     elements,
-  }
+  };
 };
 
 const p = (one, two = undefined) => {
   return two ? `<p><strong>${one}:</strong> ${two}</p>` : `<p>${one}</p>`;
-}
+};
 
 const ul = (items = []) => {
-  return `<ul>${items.map((i) => `<li>${i}</li>`).join('')}</ul>`;
-}
+  return `<ul>${items.map(i => `<li>${i}</li>`).join('')}</ul>`;
+};
 
 const element = (label, content) => {
   return {
     key: `element-${stringToKebab(label)}`,
     label,
     content,
-  }
+  };
 };
-
 
 export {
   p,
