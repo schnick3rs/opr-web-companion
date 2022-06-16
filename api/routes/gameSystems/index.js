c5d0dbe76159c01cfec285087ac9ad391b67ee99
Moritz Halke <moritz.orth@bold-ventures.de>
2022-06-16 10:32:14 +0200

    Add try catch

--- api/routes/gameSystems/index.js
+++ api/routes/gameSystems/index.js
@@ -37,30 +37,34 @@ router.get('/', async (request, response) => {
 );
 
 router.get('/:slug', async (request, response) => {
-  const { slug } = request.params;
+  try {
+    const { slug } = request.params;
 
-  const { rows } = await pool.query(
-    'SELECT ' +
-    'game_systems.id, ' +
-    'game_systems.slug, ' +
-    'game_systems.fullname, ' +
-    'game_systems.universe, ' +
-    'game_systems."portfolioLink", ' +
-    'game_systems.shortname, ' +
-    'game_systems.aberration, ' +
-    'game_systems."armyBookBuilderEnabled" ' +
-    'FROM opr_companion.game_systems ' +
-    'WHERE slug = $1',
-    [slug]
-  );
+    const { rows } = await pool.query(
+      'SELECT ' +
+      'game_systems.id, ' +
+      'game_systems.slug, ' +
+      'game_systems.fullname, ' +
+      'game_systems.universe, ' +
+      'game_systems."portfolioLink", ' +
+      'game_systems.shortname, ' +
+      'game_systems.aberration, ' +
+      'game_systems."armyBookBuilderEnabled" ' +
+      'FROM opr_companion.game_systems ' +
+      'WHERE slug = $1',
+      [slug]
+    );
 
-  if (rows.length !== 1) {
-    response.status(404).json();
-  } else {
-    const armyBook = rows[0];
+    if (rows.length !== 1) {
+      response.status(404).json();
+    } else {
+      const armyBook = rows[0];
 
-    response.set('Cache-Control', 'public, max-age=3600');
-    response.status(200).json(armyBook);
+      response.set('Cache-Control', 'public, max-age=3600');
+      response.status(200).json(armyBook);
+    }
+  } catch (e) {
+    response.status(400).json({ message: 'fail' });
   }
 });
 
